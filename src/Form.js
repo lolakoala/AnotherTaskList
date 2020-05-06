aaa76f5753b0c4b2d9541520c624dc4be4089465
Lola <lolabrennerdev@gmail.com>
2020-05-06 11:29:12 -0600

    adds outline for Form component

--- src/Form.js
+++ src/Form.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const Form = () => {
-    return (<div>
-    
-    </div>)
+    return (<form>
+        {/* title (req)- text input */}
+        {/* category (req)- dd + text input button */}
+        {/* description- textarea */}
+        {/* date due- date picker */}
+        {/* notes- textarea */}
+        {/* submit button- on submit, date added generated */}
+        {/* clear button */}
+    </form>)
 }
 
 export default Form
