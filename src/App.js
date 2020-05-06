ee410f778698787d5a1d48aea8320311b3a7eca2
Lola <lolabrennerdev@gmail.com>
2020-05-06 13:49:49 -0600

    removes render of Counter (CRA-redux)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import Form from './features/form/Form'
 
@@ -7,9 +6,6 @@ function App() {
   return (
     <div className="App">
     {/* keeping counter in as reference */}
-      <header className="App-header">
-        <Counter />
-      </header>
 
       <div className="App">
         <h1>Another Task List</h1>
