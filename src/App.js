4ca74332191f555f659ec8939ff435559fcc053c
Lola <lolabrennerdev@gmail.com>
2020-05-06 11:22:41 -0600

    adds Form component file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.css'
+import Form from './Form'
 
 function App() {
   return (
     <div className="App">
      <h1>Another Task List</h1>
      {/* pre populate button */}
-     {/* form */}
+     <Form />
      {/* lists */}
     </div>
   )
