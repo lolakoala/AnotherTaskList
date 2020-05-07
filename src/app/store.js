33369021502ea1672fcbafd8c2267de3c135b4ca
Lola <lolabrennerdev@gmail.com>
2020-05-07 13:32:30 -0600

    adds categories to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import tasksReducer from '../features/tasks/tasksSlice'
+import categoriesReducer from '../features/tasks/categoriesSlice'
 
 export default configureStore({
   reducer: {
     tasks: tasksReducer,
+    categories: categoriesReducer,
   },
 })
