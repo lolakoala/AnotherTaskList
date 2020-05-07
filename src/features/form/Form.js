e85e0b47c28d8207cafb8b6a7dee47c763c65b2a
Lola <lolabrennerdev@gmail.com>
2020-05-07 13:33:28 -0600

    adds form elements for category and priority

--- src/features/form/Form.js
+++ src/features/form/Form.js
@@ -2,21 +2,35 @@ import React, { useState } from 'react'
 import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css"
 import { addTask } from '../tasks/tasksSlice'
-import { useDispatch } from 'react-redux'
+import { selectCategories, addCategory } from '../tasks/categoriesSlice'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Form = () => {
     const dispatch = useDispatch()
+    const categories = useSelector(selectCategories)
     const [title, setTitle] = useState('')
     const [details, setDetails] = useState('')
     const [notes, setNotes] = useState('')
     const [category, setCategory] = useState('')
-    const [dueDate, setDueDate] = useState(new Date()) 
+    const [newCat, setNewCat] = useState('') 
+    const [dueDate, setDueDate] = useState(new Date())
+    const [priority, setPriority] = useState('medium') 
     const clearForm = () => {
         setTitle('')
         setDetails('')
         setNotes('')
         setCategory('')
         setDueDate(new Date())
+        setNewCat('')
+        setPriority('medium')
+    }
+    const addNewCat = () => {
+        dispatch(addCategory(newCat))
+        setCategory(newCat)
+        setNewCat('')
+    }
+    const handleRadio = event => {
+        setPriority(event.target.value)
     }
 
     return (<div>
@@ -27,23 +41,72 @@ const Form = () => {
             placeholder="Enter Task Title"
             required
         />
-        {/* if there are categories in redux, populate dropdown */}
-        {/* and render instructions to select one or submit new */}
+        {categories.length &&
+            <div>
+                <label for="cat-select">Choose a Category:</label>
+                <select 
+                    name="categories" 
+                    id="cat-select"
+                    value={category}
+                    onChange={(event) => setCategory(event.target.value)}
+                >
+                    {categories.map(cat => <option value={cat}>{cat}</option>)}
+                </select>
+            </div>
+        }
         <input
             type="category"
-            value={category}
-            onChange={event => setCategory(event.target.value)}
-            placeholder="Enter Category"
-            // conditional require
+            value={newCat}
+            onChange={event => setNewCat(event.target.value)}
+            placeholder="Enter a New Category"
+            required={!categories.length}
         />
+        <button onClick={addNewCat}>Add New Category</button>
+        <p>Select a Priority Level:</p>
+        <ul>
+            <li>
+                <label>
+                    <input
+                        type="radio"
+                        value="low"
+                        checked={priority === "low"}
+                        onChange={handleRadio}
+                    />
+                    Low
+                </label>
+            </li>
+            <li>
+                <label>
+                    <input
+                        type="radio"
+                        value="medium"
+                        checked={priority === "medium"}
+                        onChange={handleRadio}
+                    />
+                    Medium
+                </label>
+            </li>
+            <li>
+                <label>
+                    <input
+                        type="radio"
+                        value="high"
+                        checked={priority === "high"}
+                        onChange={handleRadio}
+                    />
+                    High
+                </label>
+            </li>
+        </ul>
         <textarea
             type="text"
             value={details}
             onChange={event => setDetails(event.target.value)}
             placeholder="Enter Task Details"
         />
-        {/* needs label */}
+        <label for="due-date-picker">Choose a Due Date:</label>
         <DatePicker
+            id="due-date-picker"
             selected={dueDate} 
             onChange={date => setDueDate(date)}
         />
@@ -59,6 +122,7 @@ const Form = () => {
                 title, 
                 details,
                 category,
+                priority,
                 dueDate: dueDate.toISOString(),}))
                 clearForm()
 
