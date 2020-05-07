c11e44025ccf41dffe76cbafc44e432acd3aa7f0
Lola <lolabrennerdev@gmail.com>
2020-05-07 11:17:06 -0600

    removes counter example from store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
 import tasksReducer from '../features/tasks/tasksSlice'
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     tasks: tasksReducer,
   },
 })
