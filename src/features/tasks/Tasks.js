2a834739f7f18d1d79550dff40a5c78fa7a2c13b
Lola <lolabrennerdev@gmail.com>
2020-05-09 10:47:28 -0600

    adds card component, ids, classes to tasks

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { selectTasks } from './tasksSlice'
 
-const Tasks = () => {
-    const tasks = useSelector(selectTasks)
+const Card = ({task}) => {
+    const [expanded, setExpanded] = useState(false)
+    const {
+        title,
+        details,
+        createdAt,
+        updatedAt,
+        dueDate,
+        dateCompleted,
+        notes,
+        category,
+        priority,
+    } = task
 
-    return (<div className='tasks'>
-        {tasks.map(task => <p>{task.title}</p>)}
+    return (<div className='task-card'>
+        <div className='mini-task'>
+            <p onClick={() => setExpanded(!expanded)}>{expanded ? '-' : '+'}</p>
+            {/* if completed, strike through */}
+            <h5>{title}</h5>
+            {dateCompleted ? null : <p>✓</p>}
+            <p>x</p>
+        </div>
+        {expanded ? <div className='expanded-task'>
+            {details && <p>{details}</p>}
+            <p>Date Created: {createdAt}</p>
+            {updatedAt && <p>Last Updated: {updatedAt}</p>}
+            <p>Date Due: {dueDate}</p>
+            {dateCompleted && <p>Date Completed: {dateCompleted}</p>}
+            {notes && <p>Notes: {notes}</p>}
+            {category && <p>Category: {category}</p>}
+            <p>Priority: {priority}</p>
+        </div> : null}
+
     </div>)
 }
+
+const Tasks = () => {
+    const tasks = useSelector(selectTasks)
+    // sorting
+    // show completed?
+
+    return (<div className='tasks'>
+        {tasks.map(task => <Card task={task}/>)}
+    </div>)
+}
+
 
 export default Tasks
